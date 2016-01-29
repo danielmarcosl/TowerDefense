e496fa29814aef45922477ad1ad707a9b7bf7585
Daniel <danielmarcos.to@gmail.com>
2016-01-29 12:37:33 +0100

    Made the splash screen stay for 5 seconds and then load the start scene

--- Assets/Scrips/LevelManager.cs
+++ Assets/Scrips/LevelManager.cs
@@ -4,16 +4,20 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	public float autoLoadNextLevelAfter;
+
+	void Start () {
+		if (autoLoadNextLevelAfter != 0) {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter); 
+		}
+	}
+
 	public void LoadLevel (string name) {
 		SceneManager.LoadScene (name);
 	}
 
 	public void LoadNextLevel () {
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
-	}
-
-	public void BrickDestroyed () {
-		LoadNextLevel ();
 	}
 
 	public void QuitRequest () {
