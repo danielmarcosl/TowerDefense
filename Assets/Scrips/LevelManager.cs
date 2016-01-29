2ff3014a6b30ebf8c74d0d5ad54052d452a0c0c9
Daniel <danielmarcos.to@gmail.com>
2016-01-29 12:18:31 +0100

    Import presets from BlockBreaker and added SplashScreen

---
+++ Assets/Scrips/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		SceneManager.LoadScene (name);
+	}
+
+	public void LoadNextLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void BrickDestroyed () {
+		LoadNextLevel ();
+	}
+
+	public void QuitRequest () {
+		Application.Quit ();
+	}
+}
