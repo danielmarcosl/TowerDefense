2fd9ce3814c50a29cae7e0524f56557a162389ce
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-31 20:59:33 +0100

    Created a music manager that play a audioclip based on the number of the level

---
+++ Assets/Scrips/PersistentMusic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistentMusic : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake() {
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destroy on load: " + name);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnLevelWasLoaded (int level) {
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+
+		Debug.Log("Playing clip: " +  thisLevelMusic);
+
+		if (thisLevelMusic) { // If there's some music attached
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
